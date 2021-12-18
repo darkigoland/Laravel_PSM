4a1d61e2f16f2a52e375352a91743b3c77dd4fdd
aliffazam <aliffazmi5455@gmail.com>
2021-12-18 09:18:42 +0800

    test

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $role=Auth::user()->role;
+
+        if($role=='1'){
+            return view('student');
+        }
+        if($role=='2'){
+            return view('technician');
+        }
+        else{
+            return view('dashboard');
+        }
+    }
+}
