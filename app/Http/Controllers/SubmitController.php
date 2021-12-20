f81c468aa24e9708fd9565cfb6687d95d59d8104
aliffazam <aliffazmi5455@gmail.com>
2021-12-20 10:24:00 +0800

    Revert "try"
    This reverts commit 3e656c73058a7e59f5756d88866c5392b36458cd.

---
+++ app/Http/Controllers/SubmitController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Http\Controllers\Controller;
+
+class SubmitController extends Controller 
+{
+   public function submitForm()
+   {
+     return view('submitfile');
+   }
+
+   public function submitFile(Request $request)
+   {
+       $request->file->store('public');
+       return "File has been submitted succesfully!";
+   }
+
+}
